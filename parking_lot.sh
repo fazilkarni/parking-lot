7dfc4f538cc056f26f061c3773fae97b288df4cd
fazilkarni <karni.fazil@gmail.com>
2017-11-20 15:48:11 +0530

    updated to build and execute the application from command prompt

--- parking_lot.sh
+++ parking_lot.sh
@@ -1,11 +1,12 @@
 #!/bin/bash 
+mvn install
 if [ -n "$1" ]
  then
     echo "Processing file $1"
-    java TestLoop $1
+    java -cp parking-lot-1.0.jar com.gojek.parking.client.CommandExecutor $1
  else
-  echo "Please type command to execute (exit to quit)"
-  read INPUT_COMMAND
-  java TestLoop $INPUT_COMMAND
+  #echo "Please type command to execute (exit to quit)"
+  #read INPUT_COMMAND
+  java -cp parking-lot-1.0.jar com.gojek.parking.client.CommandExecutor
   echo "You typed: $INPUT_COMMAND"
 fi 
